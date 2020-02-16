98cc0938801492c0f60c9edf71c93f3b8c5dd389
John Jung <john@fieldrotation.com>
2020-02-16 14:30:20 -0600

    Work in progress, adding dendrogram and graph output.

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 from flask import Flask, Response, render_template, request
 from io import BytesIO, StringIO
 from matplotlib import pyplot as plt
+from networkx.drawing.nx_agraph import graphviz_layout
 from openpyxl import Workbook
 from openpyxl.styles import PatternFill
 from scipy.cluster.hierarchy import dendrogram, linkage
 from werkzeug import FileWrapper
 
 import csv
+import networkx
 import numpy as np
 import requests
 import sys
@@ -110,39 +112,86 @@ def cluster():
             x += 1
         y += 1
 
-    l = linkage(data, request.form["linkage"])
-    d = dendrogram(l) 
+    l = linkage(data, request.form['linkage'])
+    d = dendrogram(
+        l,  
+        above_threshold_color='k', 
+        color_threshold=0, 
+        labels=headings_1,
+        orientation='left'
+    ) 
 
-    # sort the matrix based on dendrogram order.
-    data_out = []
-    for y in range(len(data)):
-        row_out = []
-        for x in range(len(data)):
-            row_out.append(data[d['leaves'][y]][d['leaves'][x]])
-        data_out.append(row_out)
-
-    # make an xlsx spreadsheet here to download. 
-    wb = Workbook()
-    ws = wb.active
-    for y in range(len(data_out)):            
-        for x in range(len(data_out)):            
-            cell = ws.cell(row=y+2, column=x+2, value=data_out[y][x])
-            print(get_hex(data_out[y][x], max_value))
-            color = get_hex(data_out[y][x], max_value)
-            cell.fill = PatternFill(start_color=color, end_color=color, fill_type="solid")
-
-    for i in range(len(data_out)):
-        ws.cell(row=1, column=i+2, value=headings_1[d['leaves'][i]])
-        ws.cell(row=i+2, column=1, value=headings_1[d['leaves'][i]])
-
-    f = BytesIO()
-    wb.save(f)
-    f.seek(0)
-    w = FileWrapper(f)
-    try:
-        return Response(w, mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", direct_passthrough=True)
-    except Exception as e:
-        return str(e)
+    if request.form['output'] == 'matrix':
+        # sort the matrix based on dendrogram order.
+        data_out = []
+        for y in range(len(data)):
+            row_out = []
+            for x in range(len(data)):
+                row_out.append(data[d['leaves'][y]][d['leaves'][x]])
+            data_out.append(row_out)
+    
+        # make an xlsx spreadsheet here to download. 
+        wb = Workbook()
+        ws = wb.active
+        for y in range(len(data_out)):            
+            for x in range(len(data_out)):            
+                cell = ws.cell(row=y+2, column=x+2, value=data_out[y][x])
+                print(get_hex(data_out[y][x], max_value))
+                color = get_hex(data_out[y][x], max_value)
+                cell.fill = PatternFill(start_color=color, end_color=color, fill_type="solid")
+    
+        for i in range(len(data_out)):
+            ws.cell(row=1, column=i+2, value=headings_1[d['leaves'][i]])
+            ws.cell(row=i+2, column=1, value=headings_1[d['leaves'][i]])
+    
+        f = BytesIO()
+        wb.save(f)
+        f.seek(0)
+        w = FileWrapper(f)
+        try:
+            return Response(w, mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", direct_passthrough=True)
+        except Exception as e:
+            return str(e)
+    elif request.form['output'] == 'dendrogram':
+        plt.subplots_adjust(right=0.66)
+        f = BytesIO()
+        plt.savefig(f, format='svg')
+        f.seek(0)
+        w = FileWrapper(f)
+        try:
+            r = Response(w, mimetype="mage/svg+xml", direct_passthrough=True)
+            r.headers['Content-Disposition: attachment; filename="output.svg"']
+            return r
+        except Exception as e:
+            return str(e)
+    elif request.form['output'] == 'graph':
+        g = networkx.Graph()
+        for a in range(len(labels)):
+            g.add_node(labels[a])
+            for b in range(a):
+                if data[labels[a]][labels[b]] >= cutoff:
+                    g.add_edge(labels[a], labels[b])
+                    g.add_edge(labels[b], labels[a])
+    
+        pos = graphviz_layout(g, prog='neato')
+    
+        networkx.draw_networkx_nodes(g, pos, node_color='#ffffff')
+        networkx.draw_networkx_labels(g, pos)
+        networkx.draw_networkx_edges(g, pos)
+    
+        plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+        f = BytesIO()
+        plt.savefig(f, bbox_inches='tight', edgecolor='w', facecolor='w', format='svg', pad_inches=0)
+        f.seek(0)
+        w = FileWrapper(f)
+        try:
+            r = Response(w, mimetype="mage/svg+xml", direct_passthrough=True)
+            r.headers['Content-Disposition: attachment; filename="output.svg"']
+            return r
+        except Exception as e:
+            return str(e)
+    else:
+        raise NotImplementedError
 
 if __name__ == "__main__":
     # debug only
